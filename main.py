df9e2ce6fb21bb410028b7978f44749badf22678
alsrnv <saranov92@gmail.com>
2024-06-14 15:09:24 +0300

    Remove .env file from repository and add it to .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
 import logging
 import os
-
+from dotenv import load_dotenv
 from telegram_bot import TelegramBot
+
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+KEYCLOAK_SERVER_URL = os.getenv('KEYCLOAK_SERVER_URL')
+KEYCLOAK_CLIENT_ID = os.getenv('KEYCLOAK_CLIENT_ID')
+KEYCLOAK_CLIENT_SECRET = os.getenv('KEYCLOAK_CLIENT_SECRET')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 def main():
 
@@ -12,9 +19,9 @@ def main():
 
     # Setup Telegram bot
     telegram_config = {
-        'token': '',
-        'admin_user_ids': [],
-        'allowed_user_ids': []
+        'token': TELEGRAM_TOKEN,
+        'admin_user_ids': [106876290],
+        'allowed_user_ids': [106876290]
     }
 
     # Create OpenAI Helper and Telegram bot
